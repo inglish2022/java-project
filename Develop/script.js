18c06df06a1530b370855461c705d99a59b4b476
Inglish Foust <inglish.foust5@gmail.com>
2022-03-16 21:50:19 -0400

    java startup

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,72 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var generatePassword = function() {
+
+var confirmNumber;
+var confirmChar;
+var confirmupperCase;
+var confirmlowerCase;
+
+// Character Arrays
+var numbersChar = ['0', '1', '2', '3', '4', '5', '6', '7','8', '9'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialChar = ['!', '@', '#', '$', '%', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '/', '.', ',', '<', '>', '?'];
+
+var resultArray = [];
+var userArray = [];
+
+
+
+
+//Window alerts/prompts/confirms to collect data
+var passwordLength = prompt("How many charactors would you like your password?");
+if(passwordLength < 8 || passwordLength > 128)  {
+  window.alert ("Password length must between 8-128 charactors.");
+  return
+}  else  {
+
+var Number = confirm("Would you like numbers in your password?");
+var capitol = confirm("Do you want uppercase letters in your password?");
+var special = confirm("Would you like special charactors in your password?");
+var smallCase = confirm("Would you like lowercase letters in your password?");
+
+}
+
+if (numbersChar)  {
+  resultArray = resultArray.concat(numbersChar);
+
+}
+
+if (upperCase)  {
+  resultArray = resultArray.concat(upperCase);
+}
+
+if (lowerCase)  {
+  resultArray = resultArray.concat(lowerCase);
+}
+
+if (specialChar)  {
+  resultArray = resultArray.concat(specialChar);
+}
+
+console.log(resultArray);
+
+
+for (var i = 0; i < passwordLength; i++)  {
+
+  userArray.push (resultArray[Math.floor(Math.random()  * resultArray.length)]);
+}
+
+return userArray.join("") ;
+}
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
